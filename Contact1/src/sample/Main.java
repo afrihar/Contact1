c1edb3dbfcc872f7f8f8f5fe9334f6e134238efd
afrihar <afrihar.hosting@gmail.com>
2016-12-11 01:01:52 +0700

    Coding Begin...1

---
+++ Contact1/src/sample/Main.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    public static void main(String[] args){
+
+    }
+
+    public void start(Stage stage) throws Exception{
+
+        //01. set stage title
+        stage.setTitle("Contact1");
+
+        //02. set layout
+        BorderPane layout = new BorderPane();
+        HBox hBox = new HBox();
+
+        //03. set scene
+        Scene scene = new Scene(layout, 400,300);
+
+        //04. open stage
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
