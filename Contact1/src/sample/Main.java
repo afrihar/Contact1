cf51e93dedf37eb286c73d02a884b33dd033b92d
afrihar <afrihar.hosting@gmail.com>
2016-12-11 01:18:32 +0700

    Coding Begin...2 --just show the GUI (Stage) --add button

--- Contact1/src/sample/Main.java
+++ Contact1/src/sample/Main.java
@@ -1,7 +1,9 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -21,6 +23,9 @@ public class Main extends Application{
         //02. set layout
         BorderPane layout = new BorderPane();
         HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setSpacing(10);
+        layout.setCenter(hBox);
 
         //03. set scene
         Scene scene = new Scene(layout, 400,300);
@@ -28,6 +33,12 @@ public class Main extends Application{
         //04. open stage
         stage.setScene(scene);
         stage.show();
+
+        //05. add component -> a. button
+        Button button = new Button("OK");
+        Button button1 = new Button("No");
+        hBox.getChildren().addAll(button, button1);
+
     }
 
 }
